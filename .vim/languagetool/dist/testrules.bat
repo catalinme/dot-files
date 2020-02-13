2c6e7296afa48bbc64dff54bf4764ec6507c23a7
John Doe <john.doe@fitbit.com>
2020-02-13 13:03:49 -0800

    Remove vim plugins
    Signed-off-by: John Doe <john.doe@fitbit.com>

--- .vim/languagetool/dist/testrules.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-java -cp junit.jar;LanguageTool.jar de.danielnaber.languagetool.rules.patterns.PatternRuleTest %1
-java -cp junit.jar;LanguageTool.jar de.danielnaber.languagetool.tagging.disambiguation.rules.DisambiguationRuleTest %1
-java -cp junit.jar;LanguageTool.jar de.danielnaber.languagetool.rules.bitext.BitextPatternRuleTest %1
-java -cp junit.jar;LanguageTool.jar de.danielnaber.languagetool.ValidateXMLTest %1
--- .vim/languagetool/openoffice/undeploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "Undeploying LanguageTool from OpenOffice.org..."
-unopkg remove LanguageTool-1.0.1-dev.oxt
